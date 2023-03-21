3add17c538c1fafd5dadc1424654d18396e1e376
Kumar <kumarvikash0630@gmail.com>
2023-03-21 16:11:59 +0530

    done

---
+++ src/com/Java/vikashTest.java
@@ -0,0 +1,15 @@
+package com.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class vikashTest {
+    public static void main(String args[])
+    {
+        List<String> Fruit=new ArrayList<String>();
+        Fruit.add("Banana");
+        Fruit.add("Apple");
+            System.out.println(Fruit);
+
+    }
+}
